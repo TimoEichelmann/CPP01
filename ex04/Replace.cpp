e500a0e866dacf4a6c670eb1e094766e245c9d0d
home_pc <teichelm@student.42berlin.de>
2024-10-01 17:14:56 +0200

    hello

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -1,34 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Replace.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: timo <timo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/13 18:20:40 by timo              #+#    #+#             */
-/*   Updated: 2024/10/01 16:53:06 by timo             ###   ########.fr       */
+/*   Created: 2024/10/01 16:56:34 by timo              #+#    #+#             */
+/*   Updated: 2024/10/01 16:59:20 by timo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-int main(int argc, char *argv[])
+Replace::Replace(char *file[])
 {
-	if (argc != 4)
-	{
-		std::cout << "invalid input" << std::endl;
-		return (-1);
-	}
-	std::string 	s1 = argv[2];
-	std::string 	s2 = argv[3];
-	std::string 	read;
-	std::ifstream	in(argv[1]);
-	int				index;
-	if (!in || in.bad())
-	{
-		std::cout << "bad file input" << std::endl;
-		return (-1);
-	}
-	read = in.rdbuf;
-	in.close();
+	this->in = std::ifstream temp(file);
+}
+
+Replace::find_occurence()
+{
+	
+}
+
+Replace::substitution()
+{
+	
+}
+
+Replace::~Replace()
+{
+	if (this->in)
+		this->in.close();
 }
--- ex04/Replace.hpp
+++ ex04/Replace.hpp
@@ -1,34 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: timo <timo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/13 18:20:40 by timo              #+#    #+#             */
-/*   Updated: 2024/10/01 16:53:06 by timo             ###   ########.fr       */
+/*   Created: 2024/10/01 16:52:49 by timo              #+#    #+#             */
+/*   Updated: 2024/10/01 16:56:19 by timo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
 
-
-int main(int argc, char *argv[])
-{
-	if (argc != 4)
-	{
-		std::cout << "invalid input" << std::endl;
-		return (-1);
-	}
-	std::string 	s1 = argv[2];
-	std::string 	s2 = argv[3];
-	std::string 	read;
-	std::ifstream	in(argv[1]);
-	int				index;
-	if (!in || in.bad())
-	{
-		std::cout << "bad file input" << std::endl;
-		return (-1);
-	}
-	read = in.rdbuf;
-	in.close();
+class Replace {
+		
+	public:
+		Replace(char *file[]);
+		find_occurence();
+		substitution();
+		~Replace();
+	private:
+		std::string 	s1;
+		std::string 	s2;
+		std::string 	read;
+		std::ifstream	in;
 }
